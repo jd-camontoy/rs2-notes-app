9a702f95bc79a025a1433fca568ff7a9272a3ae7
jd-camontoy <32476082+jd-camontoy@users.noreply.github.com>
2023-04-20 18:46:21 +0800

    first commit

---
+++ src/components/Header.vue
@@ -0,0 +1,11 @@
+<template>
+    <header class="header">
+        <div class="header__logo-container">
+            <img src="../assets/images/rs2-notes-logo.svg" alt="RS2 Notes App Logo" class="header__logo">
+        </div>
+        <input type="text" class="header__search-notes-field" placeholder="Search notes here">
+        <button class="btn btn--primary btn--header">
+            Logout
+        </button>
+    </header>
+</template>
---
+++ src/views/LoginView.vue
@@ -0,0 +1,27 @@
+<template>
+  <main class="main--login">
+    <div class="login">
+      <img
+        src="../assets/images/rs2-notes-logo-wo-text.svg"
+        alt="RS2 Notes App Logo"
+        class="header__logo"
+      />
+      <div class="login__inputs">
+        <div class="login__inputs--name margin-bottom-10">
+          <input type="text" placeholder="First Name" class="margin-right-10" />
+          <input type="text" placeholder="Surname" />
+        </div>
+        <input
+          class="login__input--password"
+          type="password"
+          placeholder="Password"
+        />
+      </div>
+      <button class="btn btn--primary">
+        <i class="fa-solid fa-arrow-right-to-bracket margin-right-10"></i>
+        Login to Notes
+      </button>
+      <a href="#">Register to Notes</a>
+    </div>
+  </main>
+</template>
