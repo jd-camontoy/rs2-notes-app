d6187707e6287a2af9c18f9aea65ecc503f99dec
jd-camontoy <32476082+jd-camontoy@users.noreply.github.com>
2023-04-22 03:19:10 +0800

    Quick reformat of login to form based

--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -1,23 +1,23 @@
 <script setup lang="ts">
-import { ref, computed } from 'vue';
-import { useRouter } from 'vue-router';
-import { loginUser } from '../api/requests';
+import { ref, computed } from "vue";
+import { useRouter } from "vue-router";
+import { loginUser } from "../api/requests";
 
 const name = ref("");
 const surname = ref("");
 const password = ref("");
 
-const router = useRouter()
+const router = useRouter();
 
 async function handleLogin() {
   const result = await loginUser({
     first_name: name.value,
     surname: surname.value,
-    password: password.value
+    password: password.value,
   });
   if (result.success) {
-    localStorage.setItem('user', JSON.stringify(result.data));
-    router.push('/');
+    localStorage.setItem("user", JSON.stringify(result.data));
+    router.push("/");
   } else {
     console.error(result);
   }
@@ -32,21 +32,29 @@ async function handleLogin() {
         alt="RS2 Notes App Logo"
         class="header__logo"
       />
-      <div class="login__inputs">
-        <div class="login__inputs--name margin-bottom-10">
-          <input v-model="name" type="text" placeholder="First Name" class="margin-right-10" />
-          <input v-model="surname" type="text" placeholder="Surname" />
+      <form @submit.prevent="handleLogin">
+        <div class="login__inputs">
+          <div class="login__inputs--name margin-bottom-10">
+            <input
+              v-model="name"
+              type="text"
+              placeholder="First Name"
+              class="margin-right-10"
+            />
+            <input v-model="surname" type="text" placeholder="Surname" />
+          </div>
+          <input
+            v-model="password"
+            class="login__input--password"
+            type="password"
+            placeholder="Password"
+          />
         </div>
-        <input v-model="password"
-          class="login__input--password"
-          type="password"
-          placeholder="Password"
-        />
-      </div>
-      <button @click="handleLogin" class="btn btn--primary">
-        <i class="fa-solid fa-arrow-right-to-bracket margin-right-10"></i>
-        Login to Notes
-      </button>
+        <button type="submit" class="btn btn--primary">
+          <i class="fa-solid fa-arrow-right-to-bracket margin-right-10"></i>
+          Login to Notes
+        </button>
+      </form>
       <router-link to="/register">Register to Notes</router-link>
     </div>
   </main>
